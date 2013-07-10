382eedb7a5716a7e48d61caaa39c9b2d3a1202ab
Ubuntu <ubuntu@ip-172-31-20-10.us-west-2.compute.internal>
2013-07-10 00:35:38 +0000

    Wed Jul 10 00:35:38 UTC 2013

--- web.js
+++ web.js
@@ -6,10 +6,9 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-//fs.readFileSync('index.html', function(err, data){
-//	if (err) throw err;
+
 	response.send('qsdf');
-//	});
+
 }
   
 var port = process.env.PORT || 5000;
