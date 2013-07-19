5080e5fb6852019031534db14cf849a0e71c923d
Ubuntu <ubuntu@ip-172-31-20-10.us-west-2.compute.internal>
2013-07-19 23:22:42 +0000

    bascule instance 1

--- web.js
+++ web.js
@@ -1,19 +1,16 @@
 var express = require('express');
-
-var app = express.createServer(express.logger());
-
 var fs = require('fs');
+var htmlfile = "index.html";
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-var bufferIn = fs.readFileSync('index.html');
-var bufferToString = bufferIn.toString();
-
-
-  response.send(bufferToString);
+ var html = fs.readFileSync(htmlfile).toString();
+ response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
